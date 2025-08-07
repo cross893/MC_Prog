fd3c0af4d9898bb918366dddb104893f837afb6c
cross893 <893cross893@gmail.com>
2025-08-07 21:14:02 +0500

    update

--- MC_Prog/Asm_Source_interface.h
+++ MC_Prog/Asm_Source_interface.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "Common.h"
+
 //------------------------------------------------------------------------------------------------------------
 extern "C" int makeSum(int firstValue, int secondValue);
-//extern "C" void drawPairSymbol(CHAR_INFO* screenBuffer, sPosition pos, aSymbol symbol);
 extern "C" void drawLineHorizontal(CHAR_INFO* screenBuffer, sPosition pos, aSymbol symbol);
 extern "C" void drawLineVertical(CHAR_INFO* screenBuffer, sPosition pos, aSymbol symbol);
 extern "C" void showColors(CHAR_INFO* screenBuffer, sPosition pos, CHAR_INFO symbol);
 extern "C" void clearArea(CHAR_INFO* screenBuffer, sAreaPosition areaPos, aSymbol symbol);
 extern "C" int drawBotText(CHAR_INFO* screenBuffer, sTextPosition pos, const wchar_t* str);
 extern "C" void drawBotLimitedText(CHAR_INFO* screenBuffer, sTextPosition pos, const wchar_t* str, unsigned short limit);
-//------------------------------------------------------------------------------------------------------------
